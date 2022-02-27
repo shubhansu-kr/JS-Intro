bf6206f551c404fc09e73cbb783f3c5a4ade118b
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-27 19:07:12 +0530

    Exercise - Functions

---
+++ 01_Exercise.js
@@ -0,0 +1,47 @@
+// Calculator with function
+
+const num1 = 10, num2 = 20, num3 = 30;
+
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function sub(num1, num2) {
+    return num2 - num1;
+};
+
+function mul(num1, num2) {
+    return num1 * num2;
+};
+
+function div(num1, num2) {
+    return num2 / num1;
+}
+
+function funky(num1, num2, num3) {
+    return num2 * num3 + num1;
+}
+
+
+// In this lab, you're supposed to create 5 functions:
+
+// add(num1, num2)
+// This function should return the sum of num1 and num2
+
+// sub(num1, num2)
+// This function should subtract num2 from num1 
+// (i.e. num2 - num1) and return the result
+
+// mul(num1, num2)
+// This function should multiply num1 and num2 and return the 
+// result
+
+// div(num1, num2)
+// This function should divide num2 by num1 (i.e. num2 / num1) 
+// and return the result
+
+// funky(num1, num2, num3)
+// This should multiply num2 and num3 and add num1 to this result. The final result 
+// should be returned.
+
+// All the best!

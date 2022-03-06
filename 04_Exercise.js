9afa9a9478db3d9ff939376d2681fac2e1d4fc90
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-06 23:12:57 +0530

    Exercise - Objects

---
+++ 04_Exercise.js
@@ -0,0 +1,18 @@
+// In this lab, you have to build a JS object.
+
+// This object should be named 'myOwnObject'
+// You should have a property called myAge which should be a number
+// You should have a property called myName which should be a string
+// You should have another property call myFriend which should be an 
+// object that should include myFriendAge as a property with their age 
+// as a number
+
+// All the best!
+
+const myOwnObject = {
+    myAge: 18,
+    myName: 'Shubhansu',
+    myFriend: {
+        myFriendAge: 19
+    }
+}

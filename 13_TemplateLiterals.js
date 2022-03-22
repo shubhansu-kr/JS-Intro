99a48407f8ef18ebabcea6f1389ecba5b0757684
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-22 15:42:03 +0530

    Literals and Template

---
+++ 13_TemplateLiterals.js
@@ -0,0 +1,17 @@
+// Template - Literals 
+
+// Using backTicks to create strings ; 
+
+// const myData = `My name is Shubh, 
+// My age is 18 ` ;
+
+// console.log(myData);
+
+// Concatination in literals ; 
+const myData = {
+    name: 'shubhansu ',
+    age: 18
+}
+
+const myString = `My name is ${myData.name}, and age ${myData.age}`;
+console.log(myString);
